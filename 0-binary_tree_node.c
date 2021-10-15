7deee2eb30373850dc012498f1576c96018e7906
Grace Fallon <graceleefallon@gmail.com>
2021-10-15 11:13:23 -0500

    Style changes; language adjustments

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -2,7 +2,7 @@
 
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
-    new *newNode = malloc(sizeof(binary_tree_t));
+    binary_tree_t *newNode = malloc(sizeof(binary_tree_t));
     
     newNode -> n = value;
     newNode -> parent = parent;
