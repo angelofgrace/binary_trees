d23b264a4efd2908f64938d130a33e5b4b185bd3
angelofgrace <graceleefallon@gmail.com>
2021-10-18 21:41:03 +0000

    Remove trailing whitespace for linter

--- 8-binary_tree_postorder.c
+++ 8-binary_tree_postorder.c
@@ -8,7 +8,7 @@
 * @func: function to be executed on data within each node
 *
 * Return: none
-*/ 
+*/
 void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
 {
 	if (tree == NULL || func == NULL)
