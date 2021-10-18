ba49d1474aa29e3408298ff2f1c9161f2f85f4c5
angelofgrace <graceleefallon@gmail.com>
2021-10-18 22:51:09 +0000

    Remove addition of extra count for root connection; betty style adjustments

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -1,5 +1,12 @@
 #include "binary_trees.h"
 
+/**
+* binary_tree_height - find the height of a binary tree
+*
+* @tree: pointer to the root node of the binary tree
+*
+* Return: unsigned integer, measure of the binary tree's height
+*/
 size_t binary_tree_height(const binary_tree_t *tree)
 {
 	size_t leftHeight;
@@ -17,10 +24,10 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	/* return the larger of the two subtrees */
 	if (leftHeight > rightHeight)
 	{
-		return (leftHeight + 1);
+		return (leftHeight);
 	}
 	else
 	{
-		return (rightHeight + 1);
+		return (rightHeight);
 	}
- }
+}
